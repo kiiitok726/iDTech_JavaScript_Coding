5ae995daf3fc4985af06fba9e1e6d7951d2a6a1c
Kaito Kanazawa <kaito.kanazawa@gmail.com>
2022-07-21 10:15:45 -0700

    Assignment file added

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,28 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var name = "Anakin Skywalker"
+var master = "Obi-wan Kenobi"
+var home = "Tatooine"
+var saberColor = "green"
+
+// TODO: Create 3-number variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+var age = 24
+var experience = 5
+var visitedPlanets = 3
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+var weapons = ["lightsaber", "force"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+var loyalty = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("I'm", name, "currently", age, "years old.")
+console.log(experience, "years since leaving my home planet", home, "with Master", master + ",")
+console.log("I've visited", visitedPlanets, "planets to save them all from the dark side.")
+console.log("I use my", saberColor, weapons[0], "and the", weapons[1], "to fight.")
+console.log("It is", loyalty, "that I am loyal to the jedi, but I certainly question them at times...")
